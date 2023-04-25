4c73fa3ec8c8673d06b31b4c880e3a87b7076751
bmrx <bymax845@gmail.com>
2023-04-25 11:14:14 -0400

    hola
    creacion

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app color="blue" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Facturacion</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-account-plus</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-login-variant</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-layout>
+    <v-navigation-drawer app v-model="drawer" temporary>
+      <v-list
+          density="compact"
+          nav
+        >
+        <v-icon>mdi-view-dashboard</v-icon>
+        <v-list-item >home</v-list-item>
+        <v-list-item >EMS</v-list-item>
+          <v-list-item prepend-icon="mdi-view-dashboard">Tradiocional</v-list-item>
+          <v-list-item prepend-icon="mdi-forum">casillas</v-list-item>
+          <v-list-item prepend-icon="mdi-view-dashboard">Encomienda</v-list-item>
+          <v-list-item prepend-icon="mdi-forum">ECA/PLIEGOS</v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+  </v-layout>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+<script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: 'App',
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    drawer:false
+  }),
+};
+</script>
